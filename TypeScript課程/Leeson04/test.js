dd15a824c549bb9228ec8722f026b5250da224ec
Hubert <usogood717@gmail.com>
2020-10-08 19:13:34 +0800

    add Lesson01-04

---
+++ TypeScript課程/Leeson04/test.js
@@ -0,0 +1,4 @@
+var myname = "koma";
+console.log("My name is: " + myname);
+var myage = 25;
+console.log("My age is:" + myage);
---
+++ TypeScript課程/Leeson04/test.ts
@@ -0,0 +1,7 @@
+let myname: string = "koma"
+console.log("My name is: " + myname)
+
+// myname = 200; // 編譯錯誤,類型不匹配
+
+var myage: number = 25
+console.log("My age is:" + myage)
