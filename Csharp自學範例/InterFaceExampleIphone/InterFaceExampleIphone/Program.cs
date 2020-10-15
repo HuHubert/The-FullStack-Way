a68be6d54e4c12be7f45ea3bd8ce75bf5be6765f
Hubert <usogood717@gmail.com>
2020-10-15 14:13:49 +0800

    C#自學範例

---
+++ Csharp自學範例/InterFaceExampleIphone/InterFaceExampleIphone/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace InterFaceExampleIphone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new PhoneUser(new EricssonPhone());
+
+            user.UsePhone();
+        }
+    }
+
+
+    class PhoneUser
+    {
+
+        private Iphone _phone;
+
+        public PhoneUser(Iphone phone)
+        {
+            _phone = phone;
+        }
+        public void UsePhone()
+        {
+
+            _phone.Dail();
+            _phone.PickUp();
+            _phone.Send();
+            _phone.Receive();
+        }
+    
+
+
+    }
+
+
+
+
+
+    interface Iphone
+    {
+
+        void Dail();
+        void PickUp();
+        void Send();
+        void Receive();
+
+
+    }
+
+
+    class NokiaPhone : Iphone
+    {
+        public void Dail()
+        {
+            Console.WriteLine("Nokia is calling");
+        }
+
+        public void PickUp()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void Receive()
+        {
+            Console.WriteLine("Receive Message");
+        }
+
+        public void Send()
+        {
+            Console.WriteLine("Message");
+        }
+    }
+
+    class EricssonPhone : Iphone
+    {
+        public void Dail()
+        {
+            Console.WriteLine("Ericsson is calling");
+
+        }
+
+        public void PickUp()
+        {
+            Console.WriteLine("E Hello");
+        }
+
+        public void Receive()
+        {
+            Console.WriteLine("E Receive Message");
+        }
+
+        public void Send()
+        {
+            Console.WriteLine("E Message");
+        }
+    }
+}
---
+++ Csharp自學範例/overWrite/overWrite/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace overWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v = new Car();
+            v.Run();
+            Console.WriteLine(v.Speed); 
+            
+        }
+    }
+
+
+
+    class Vehicle
+    {
+        private int _speed;
+
+
+        public virtual int Speed
+        {
+
+            get { return _speed; }
+            set { _speed = value; }
+
+        }
+
+
+        public virtual void Run()
+        {
+            Console.WriteLine("I'm Running");
+
+            _speed = 100;
+        }
+    }
+
+
+
+
+    class Car : Vehicle
+    {
+        private int _rpm;
+
+        public override int Speed {
+
+            get { return _rpm / 100; }
+
+
+            set { _rpm = value * 100; }
+
+        }
+
+        public override void Run()
+        {
+
+            Console.WriteLine("Car is running");
+
+            _rpm = 5000;
+
+        }
+    }
+
+
+
+    class RaceCar : Car
+    {
+        public override void Run() {
+
+
+            Console.WriteLine("Racecar is running"); 
+
+        }
+
+
+
+    }
+}
