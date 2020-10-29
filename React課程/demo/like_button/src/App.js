f44af3fe40f6b9cc75dac9e7abbed721b286a794
Hubert <usogood717@gmail.com>
2020-10-29 15:28:20 +0800

    組件的撰寫與引用

--- React課程/demo/like_button/src/App.js
+++ React課程/demo/like_button/src/App.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button,{Nav} from './Button'
+
+
+
+
+// //class方法
+// class Nav extends Component {
+//   constructor() {
+//     super()
+//   }
+
+//   render(){
+//     return (
+//       <div style={{color: 'white', backgroundColor:'black'}}>Skipper</div>
+//     )
+//   }
+// }
+
+
+// //函數方法
+// const Button = function(){
+//   return (
+//     <button type="button">來自Buttony組件
+//     </button>
+//   )
+// }
+
+
 
  class App extends React.Component {
    constructor (){
@@ -23,9 +51,16 @@ import './App.css';
 
     console.log("render函數觸發了")
     return (
+      
+      <div>
 
+      <Nav/>
 
-      <button type="button" style={this.state.like ? {color: "red"}:{color:"black"}}
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+
+     <button type="button" style={this.state.like ? {color: "red"}:{color:"black"}}
       onClick={() => this.handleClick()}
       >
 
@@ -35,6 +70,11 @@ import './App.css';
 
 
       </button>
+
+      </div>
+
+
+
     
       )
   }
