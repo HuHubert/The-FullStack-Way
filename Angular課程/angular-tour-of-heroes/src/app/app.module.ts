99bb22d12f5728bd00ff6fd8abecb81d2bfa8808
Hubert <usogood717@gmail.com>
2020-11-17 16:48:26 +0800

    service應用＆依賴注入

--- Angular課程/angular-tour-of-heroes/src/app/app.module.ts
+++ Angular課程/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HerosComponent } from './heros/heros.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HerosComponent
+    HerosComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular課程/angular-tour-of-heroes/src/app/heros/heros.component.ts
+++ Angular課程/angular-tour-of-heroes/src/app/heros/heros.component.ts
@@ -1,21 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
+import { MessageService } from '../message.service';
+
+// import {HEROES} from '../mock-heroes'
 
 @Component({
-  selector: 'app-heros',
+  selector: 'app-heroes',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
 
-  hero : Hero = {
-    id :1 ,
-    name : 'Windstorm'
-  };
+  heroes : Hero[];
 
-  constructor() { }
+  selectedHero: Hero;
+
+
+
+  constructor(private heroService: HeroService,private messageService: MessageService) { }
 
   ngOnInit(): void {
-  }
+    this.getHeroes();
+  };
 
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+  this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  };
+
+  // getHeroes(): void {
+  // this.heroes = this.heroService.getHeroes();
+  // };
+  getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  };
 }
