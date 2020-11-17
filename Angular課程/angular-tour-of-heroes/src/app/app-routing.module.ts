869ab604ea11655066bb7c327ef0c218b908e9a0
Hubert <usogood717@gmail.com>
2020-11-17 18:33:33 +0800

    route

--- Angular課程/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ Angular課程/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HerosComponent } from './heros/heros.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
 
 
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'heroes', component: HerosComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
